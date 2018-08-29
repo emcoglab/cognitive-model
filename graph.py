fd73d2c1d15249e9faa73cb3a7f2f14bcc789e94
Cai <c.wingfield@lancaster.ac.uk>
2018-08-29 16:01:56 +0100

    Warn don’t assert

--- graph.py
+++ graph.py
@@ -149,8 +149,8 @@ class Graph:
         :return:
         """
         ignoring_long_edges = (ignore_edges_longer_than is not None)
-        if not ignoring_long_edges:
-            assert not keep_at_least_n_edges
+        if not ignoring_long_edges and keep_at_least_n_edges:
+            logger.warning(f"Requested to keep {keep_at_least_n_edges} but not pruning. This parameter is therefore being ignored.")
 
         edges_to_keep = defaultdict(lambda: SortedSet(key=lambda x: x[1]))
 
