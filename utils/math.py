5f54d48a7ff32b2e1f4a57bb3e17c86236986312
Cai <c.wingfield@lancaster.ac.uk>
2018-08-30 11:36:42 +0100

    Extract `mean` into a library function.

---
+++ utils/math.py
@@ -0,0 +1,20 @@
+"""
+===========================
+Math utils.
+===========================
+
+Dr. Cai Wingfield
+---------------------------
+Embodied Cognition Lab
+Department of Psychology
+University of Lancaster
+c.wingfield@lancaster.ac.uk
+caiwingfield.net
+---------------------------
+2018
+---------------------------
+"""
+
+
+def mean(*items):
+    return sum(items) / len(items)
