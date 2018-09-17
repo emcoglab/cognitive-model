dfc2fb19dac09f1c7648281774bc5a733412e110
Cai <c.wingfield@lancaster.ac.uk>
2018-09-17 14:09:19 +0100

    Run category production comparison on multiple folders at once.

---
+++ utils/exceptions.py
@@ -0,0 +1,21 @@
+"""
+===========================
+Exception classes.
+===========================
+
+Dr. Cai Wingfield
+---------------------------
+Embodied Cognition Lab
+Department of Psychology
+University of Lancaster
+c.wingfield@lancaster.ac.uk
+caiwingfield.net
+---------------------------
+2018
+---------------------------
+"""
+
+
+class ParseError(Exception):
+    """Represents an error in parsing a string."""
+    pass
