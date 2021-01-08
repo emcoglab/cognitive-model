4851e3382244defe8c40edefb05f11c7364f1876
Cai Wingfield <c.wingfield@lancaster.ac.uk>
2021-01-08 16:19:32 +0000

    Set in InterComponentMapping class

--- combined_cognitive_model.py
+++ combined_cognitive_model.py
@@ -17,11 +17,13 @@ caiwingfield.net
 
 from __future__ import annotations
 
-from typing import List, Optional
+from collections import defaultdict
+from typing import List, Optional, Set, Dict, DefaultDict
+from logging import getLogger
 
 from numpy import lcm
 
-from breng_ameng.dialect_dictionary import ameng_to_breng, breng_to_ameng
+from .sensorimotor_norms.breng_translation.dictionary.dialect_dictionary import ameng_to_breng
 from .basic_types import ActivationValue, Component, Size, Item, SizedItem
 from .buffer import WorkingMemoryBuffer
 from .events import ItemActivatedEvent, ItemEvent, ModelEvent
@@ -29,6 +31,126 @@ from .linguistic_components import LinguisticComponent
 from .sensorimotor_components import SensorimotorComponent
 from .utils.exceptions import ItemNotFoundError
 from .utils.iterable import partition
+
+_logger = getLogger(__name__)
+
+
+class InterComponentMapping:
+    def __init__(self,
+                 linguistic_vocab: Set[str],
+                 sensorimotor_vocab: Set[str],
+                 ):
+        # The mapping is not symmetric, so we must store both directions
+        linguistic_to_sensorimotor: DefaultDict[str, Set[str]] = defaultdict(set)
+        sensorimotor_to_linguistic: DefaultDict[str, Set[str]] = defaultdict(set)
+
+        # Assume sensorimotor norms have already been translated into BrEng.
+
+        _logger.info("Setting up inter-component mapping")
+
+        # linguistic --> sensorimotor direction
+        # This is the easier direction, as we only ever map one -> one
+        for linguistic_term in linguistic_vocab:
+
+            if linguistic_term in sensorimotor_vocab:
+                # Term exists in both places: just map directly
+                linguistic_to_sensorimotor[linguistic_term].add(linguistic_term)
+
+            else:
+                # Can't map directly
+                # Attempt translation:
+                breng_linguistic_terms = ameng_to_breng.best_translations_for(linguistic_term)
+                if len(breng_linguistic_terms) == 0:
+                    # No translations, can't map
+                    continue
+                # If there is at least one option, pick the best one by BrEng preference.
+                linguistic_to_sensorimotor[linguistic_term].add(breng_linguistic_terms[0])
+
+        # sensorimotor --> linguistic direction
+        for sensorimotor_term in sensorimotor_vocab:
+
+            # Complexity here comes with the need to deal with the following two cases:
+            #   1a. ANAESTHETISE -> anaesthetise (anaesthetise >> anesthetise)
+            #   1b. ANESTHETISE  -> anaesthetise
+            # and
+            #   2a. COURGETTE -> courgette (courgette ~ zucchini)
+            #   2b. ZUCCHINI  -> zucchini
+            # This means we need to check first if there may be collisions (both the above cases), and then if so,
+            # check which of the two sub-cases we're in.
+            potential_collisions = ameng_to_breng.best_translations_for(sensorimotor_term)
+            if len(potential_collisions) > 1:
+                if sensorimotor_term not in potential_collisions:
+                    # We're in the first case (1b)
+                    # So we're safe to map to try and map to the best available candidate
+                    for linguistic_candidate in potential_collisions:
+                        if linguistic_candidate in linguistic_vocab:
+                            sensorimotor_to_linguistic[sensorimotor_term].add(linguistic_candidate)
+                            break
+                    # In case none of the candidates are available, we can try to map to the original term, else we just
+                    # fail.
+                    if sensorimotor_term in linguistic_vocab:
+                        sensorimotor_to_linguistic[sensorimotor_term].add(sensorimotor_term)
+                    else:
+                        _logger.warning(f"Failed to find map for {sensorimotor_term}")
+                        continue
+                else:
+                    # We're in the first sub-case (1a) or the second (2a, 2b).
+                    # Either way, we're safe to map directly to the same term, if possible.
+                    if sensorimotor_term in linguistic_vocab:
+                        sensorimotor_to_linguistic[sensorimotor_term].add(sensorimotor_term)
+                    else:
+                        _logger.warning(f"Failed to find map for {sensorimotor_term}")
+                        continue
+
+            # Otherwise, we may have just one candidate:
+            elif len(potential_collisions) == 1:
+                linguistic_candidate = potential_collisions.pop()
+                if linguistic_candidate in linguistic_vocab:
+                    sensorimotor_to_linguistic[sensorimotor_term].add(linguistic_candidate)
+                else:
+                    _logger.warning(f"Failed to find map for {sensorimotor_term}")
+                    continue
+
+            # Finally, we may have no candidates, in which case we can try mapping directly
+            else:
+                if sensorimotor_term in linguistic_vocab:
+                    sensorimotor_to_linguistic[sensorimotor_term].add(sensorimotor_term)
+                else:
+                    _logger.warning(f"Failed to find map for {sensorimotor_term}")
+                    continue
+
+        # Almost every item will be mapped to itself, so we don't need to explicitly remember that. Let's save memory!
+        linguistic_identity = [
+            s
+            for s, ts in linguistic_to_sensorimotor.items()
+            if ts == {s}
+        ]
+        sensorimotor_identity = [
+            s
+            for s, ts in sensorimotor_to_linguistic.items()
+            if ts == {s}
+        ]
+        for i in linguistic_identity:
+            del linguistic_to_sensorimotor[i]
+        for i in sensorimotor_identity:
+            del sensorimotor_to_linguistic[i]
+
+        # Freeze and set
+        self.linguistic_to_sensorimotor: Dict[str, Set[str]] = dict(linguistic_to_sensorimotor)
+        self.sensorimotor_to_linguistic: Dict[str, Set[str]] = dict(sensorimotor_to_linguistic)
+
+    @staticmethod
+    def linguistic_equivalents(linguistic_term: str) -> Set[str]:
+        """Words which are equi-preferred to the supplied word."""
+        # Check if it has any different translations
+        translations = set(ameng_to_breng.best_translations_for(linguistic_term))
+        if linguistic_term in translations:
+            # Group of equivalents
+            return translations
+        else:
+            # translations, if any, are not equivalent
+            return {linguistic_term}
+    # No one-many mappings in the other direction; each sensorimotor equivalence class is of size 1
 
 
 class InteractiveCombinedCognitiveModel:
@@ -70,6 +192,12 @@ class InteractiveCombinedCognitiveModel:
         # The shared buffer does not affect the activity within either component or between them.
         self.linguistic_component.propagator.postsynaptic_guards.extend([])
         self.sensorimotor_component.propagator.postsynaptic_guards.extend([])
+
+        # Inter-component mapping logic
+        self._mapping = InterComponentMapping(
+            linguistic_vocab=self.linguistic_component.available_labels,
+            sensorimotor_vocab=self.sensorimotor_component.available_labels,
+        )
 
     @property
     def clock(self) -> int:
@@ -137,42 +265,48 @@ class InteractiveCombinedCognitiveModel:
             if event.fired:
                 # Use label lookup from source component
                 linguistic_label = self.linguistic_component.propagator.idx2label[event.item.idx]
-                try:
-                    self.sensorimotor_component.propagator.schedule_activation_of_item_with_label(
-                        label=(
-                            # Use the linguistic label if it's available
-                            linguistic_label
-                            if linguistic_label in self.sensorimotor_component.available_labels
-                            # Otherwise try the Americanized version
-                            else breng_to_ameng.best_translation_for(linguistic_label)
-                        ),
-                        activation=event.activation * self._inter_component_attenuation,
-                        arrival_time=event.time + self._lc_to_smc_delay)
-                except ItemNotFoundError:
-                    # Linguistic item was not found in Sensorimotor component
-                    pass
+                # If there are mappings, use them
+                if linguistic_label in self._mapping.linguistic_to_sensorimotor:
+                    for sensorimotor_label in self._mapping.linguistic_to_sensorimotor[linguistic_label]:
+                        self.sensorimotor_component.propagator.schedule_activation_of_item_with_label(
+                            label=sensorimotor_label,
+                            activation=event.activation * self._inter_component_attenuation,
+                            arrival_time=event.time + self._lc_to_smc_delay)
+                # Otherwise just try the fallback of direct mapping
+                else:
+                    try:
+                        self.sensorimotor_component.propagator.schedule_activation_of_item_with_label(
+                            label=linguistic_label,
+                            activation=event.activation * self._inter_component_attenuation,
+                            arrival_time=event.time + self._lc_to_smc_delay)
+                    # If the direct mapping is not possible, just forget it
+                    except ItemNotFoundError:
+                        continue
         for event in smc_activation_events:
             # Only transmit to other component if it fired.
             if event.fired:
                 # Use label lookup from source component
                 sensorimotor_label = self.sensorimotor_component.propagator.idx2label[event.item.idx]
-                try:
-                    self.linguistic_component.propagator.schedule_activation_of_item_with_label(
-                        label=(
-                            sensorimotor_label
-                            # Use the linguistic label if it's available
-                            if sensorimotor_label in self.linguistic_component.available_labels
-                            # Otherwise try the Britishised version
-                            else ameng_to_breng.best_translation_for(sensorimotor_label)
-                        ),
-                        activation=event.activation * self._inter_component_attenuation,
-                        arrival_time=event.time + self._smc_to_lc_delay)
-                except ItemNotFoundError:
-                    # Sensorimotor item was not found in Linguistic component
-                    pass
+                # If there are mappings, use them
+                if sensorimotor_label in self._mapping.sensorimotor_to_linguistic:
+                    for linguistic_label in self._mapping.sensorimotor_to_linguistic[sensorimotor_label]:
+                        self.linguistic_component.propagator.schedule_activation_of_item_with_label(
+                            label=linguistic_label,
+                            activation=event.activation * self._inter_component_attenuation,
+                            arrival_time=event.time + self._smc_to_lc_delay)
+                # Otherwise just try the fallback of direct mapping
+                else:
+                    try:
+                        self.linguistic_component.propagator.schedule_activation_of_item_with_label(
+                            label=sensorimotor_label,
+                            activation=event.activation * self._inter_component_attenuation,
+                            arrival_time=event.time + self._smc_to_lc_delay)
+                    # If the direct mapping is not possible, just forget it
+                    except ItemNotFoundError:
+                        continue
 
         return (
-                decay_events
-                + lc_activation_events + lc_other_events
-                + smc_activation_events + smc_other_events
+            decay_events
+            + lc_activation_events + lc_other_events
+            + smc_activation_events + smc_other_events
         )
